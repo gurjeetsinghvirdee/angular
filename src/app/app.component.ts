ab771868449cd8e937f98ba84e21bf3520e3e366
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-07-02 16:57:40 +0530

    working with style components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css'] original css
+
+  // You can use style as same as html file
+  styles: [`
+    h2 {
+      color: blue;
+    }
+  `]
 })
 export class AppComponent {
   
